1151c480950d0cf6ca382ee15852f56025b945c4
Ian Kahrilas <ijk60068@gmail.com>
2021-02-01 09:49:10 -0600

    Finish reanalyses for behavioral data.

--- scripts/analyses.R
+++ scripts/analyses.R
@@ -1,9 +1,10 @@
 # set up
+library(checkpoint)
 library(tidyverse)
 library(lmerTest)
-library(emmeans)
 library(effectsize)
 library(MKinfer)
+library(emmeans)
 
 # read in long data
 dat <- read_csv("data/Analyses/dat_long.csv")
@@ -25,35 +26,90 @@ dat$Income <- factor(dat$Income, levels = c("$10,000 to $20,000",
 ###ANALYSES###
 ##############
 
-# comparison of income between bilingual and monolingual gruops
-bi_income <- dat %>% 
+# comparison of income between bilingual and monolingual groups
+bi_income <- dat[!duplicated(dat$PID),] %>% 
   filter(Group == 2) %>% 
   select(Income) %>% 
   pull()
 
-mono_income <- dat %>% 
+mono_income <- dat[!duplicated(dat$PID),] %>% 
   filter(Group == 1) %>% 
   select(Income) %>% 
   pull()
 
-## bootstrapped t test since distribution 
+## bootstrapped t test since distribution is wacky!
 ses_mod <- boot.t.test(x = as.numeric(mono_income), 
             y = as.numeric(bi_income),
             R = 10000)
 ### there are significant differences in income between the two groups
+ses_mod
 
-# accuracy
+# accuracy blocked trials
 acc_mod <- lmer(accuracy ~ Group*condition + as.numeric(Income) + (1|PID),
                      data = dat %>% 
                      filter(condition %in% c("congruent", "incongruent")))
 
-anova(acc_mod)
+anova(acc_mod, type = 1) # type 1 appears to be what Laura ran in the initial analyses
+eta_squared(anova(acc_mod, type = 1))
 
-## post-hoc comparisons
-acc_mod %>% 
-  emmeans(~ Group) %>% 
-  contrast("pairwise", by = "Group") %>% 
-  summary(by = NULL, adjust = "holm")
+emmeans(acc_mod, data = filter(dat, condition %in% c("congruent", "incongruent")), ~ Group*condition) %>% 
+  contrast("pairwise", by = "condition", adjust = "holm")
+
+# response time blocked trials
+rt_mod <- lmer(response_time ~ Group*condition + as.numeric(Income) + (1|PID),
+                data = dat %>% 
+                  filter(condition %in% c("congruent", "incongruent")))
+
+anova(rt_mod, type = 1)
+eta_squared(anova(rt_mod, type = 1))
+
+# accuracy mixed trials
+acc_mix_mod <- lmer(accuracy ~ Group*condition + as.numeric(Income) + (1|PID),
+                data = dat %>% 
+                  filter(condition %in% c("mix_congruent", "mix_incongruent")))
+
+anova(acc_mix_mod, type = 1)
+eta_squared(anova(acc_mix_mod, type = 1))
+
+emmeans(acc_mix_mod, data = filter(dat, condition %in% c("congruent", "incongruent")), ~ Group*condition) %>% 
+  contrast("pairwise", by = "condition", adjust = "holm")
+
+# response time mixed trials
+rt_mixed_mod <- lmer(response_time ~ Group*condition + as.numeric(Income) + (1|PID),
+               data = dat %>% 
+                 filter(condition %in% c("mix_congruent", "mix_incongruent")))
+
+anova(rt_mixed_mod, type = 1)
+eta_squared(anova(rt_mixed_mod, type = 1))
+
+# accuracy switch
+acc_switch_mod <- lmer(accuracy ~ Group*condition + as.numeric(Income) + (1|PID),
+                    data = dat %>% 
+                      filter(condition %in% c("switch_no", "switch_yes")))
+
+anova(acc_switch_mod, type = 1)
+eta_squared(anova(acc_switch_mod, type = 1))
+
+emmeans(acc_mix_mod, data = filter(dat, condition %in% c("congruent", "incongruent")), ~ Group*condition) %>% 
+  contrast("pairwise", by = "condition", adjust = "holm")
+
+# response time switch
+rt_switch_mod <- lmer(response_time ~ Group*condition + as.numeric(Income) + (1|PID),
+                       data = dat %>% 
+                         filter(condition %in% c("switch_no", "switch_yes")))
+
+anova(rt_switch_mod, type = 1)
+eta_squared(anova(rt_switch_mod, type = 1))
+
+emmeans(acc_mix_mod, data = filter(dat, condition %in% c("congruent", "incongruent")), ~ Group*condition) %>% 
+  contrast("pairwise", by = "condition", adjust = "holm")
+
+
+# old code that used to work
+# acc_mod %>% 
+#   emmeans(~ Group) %>% 
+#   contrast("pairwise", by = "Group") %>% 
+#   summary(by = NULL, adjust = "holm")
 
 
 # N450 analysis
